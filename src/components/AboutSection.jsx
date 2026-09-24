a7465048862c28772730d276e0756ec80e8cb6f3
Ivan Torres <itcache1@gmail.com>
2026-09-24 14:02:54 -0500

    Updated resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/ivan resumepdf-1 copy.pdf"
+                href="/ivan_torresResume.pdf"
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
