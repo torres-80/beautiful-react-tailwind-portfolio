c79040c4dc7855f18162b28e5d9250160b90347b
Ivan Torres <itcache1@gmail.com>
2026-09-24 16:27:08 -0500

    Centered the scroll icon

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -34,7 +34,7 @@ export const HeroSection = () => {
         </div>
       </div>
 
-      <div className="absolute bottom-8 left=1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 flex flex-col items-center animate-bounce">
         <span className="center text-sm text-muted-foreground mb-2">
           {" "}
           Scroll
